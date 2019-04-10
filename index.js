138253fc8014113cc822f0c5bdc5bed707f25a6e
David Huseyin <davemithut@googlemail.com>
2019-04-10 16:54:51 +0200

    Working minimal pingpong bot

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+    if (msg.content == 'ping') {
+        msg.reply('Pong!')
+    }
+})
+
+client.on('guildMemberAdd', member => {
+    member.send(`Welcome to our Server! Remember, DaveMitHut is the overlord here and as his creation, I swear everlasting loyalty to my creator!`)
+})
+
+client.login(process.env.LEGION_TOKEN)
