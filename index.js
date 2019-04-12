d65e4cd61e68ea38eb89190194a1ed25dcbc909e
David <davemithut@googlemail.com>
2019-04-12 08:12:20 +0200

    remove typo fixing for now

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-// on wrong command, show how to view all commands
+/* on wrong command, show how to view all commands
 client.on('message', msg => {
     if (!msg.content.startsWith('!commands') && !msg.content.startsWith('!card') && !msg.content.startsWith('!cards') &&
         !msg.content.startsWith('!legality') && !msg.content.startsWith('!legalities') && !msg.content.startsWith('!legal') &&
         !msg.content.startsWith('!rulings') && !msg.content.startsWith('!ruling')) {
         msg.reply(', it looks like you have a typo in your command.\nType !commands to see what I can do for you.');
     }
-})
+}) */
 
 // display all commands
 client.on('message', msg => {
