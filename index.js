bb635658b0bc150cd079123397ebeb8e5e034317
David <davemithut@googlemail.com>
2019-04-12 11:55:27 +0200

    debug typo fixing

--- index.js
+++ index.js
@@ -8,22 +8,25 @@ client.on('ready', () => {
 
 //on wrong command, show how to view all commands
 /*client.on('message', msg => {
-    if (!(msg.content.startsWith('!commands')) && !(msg.content.startsWith('!card')) && !(msg.content.startsWith('!cards')) &&
-        !(msg.content.startsWith('!legality')) && !(msg.content.startsWith('!legalities')) && !(msg.content.startsWith('!legal')) &&
-        !(msg.content.startsWith('!rulings')) && !(msg.content.startsWith('!ruling')) && !(msg.content.startsWith('!dice')) &&
-        !(msg.content.startsWith('!roll'))) {
-        msg.reply(', it looks like you have a typo in your command.\nType !commands to see what I can do for you.');
+    if (msg.content.startsWith('!')) {
+        if (!(msg.content.startsWith('!commands')) && !(msg.content.startsWith('!card')) && !(msg.content.startsWith('!cards')) &&
+            !(msg.content.startsWith('!legality')) && !(msg.content.startsWith('!legalities')) && !(msg.content.startsWith('!legal')) &&
+            !(msg.content.startsWith('!rulings')) && !(msg.content.startsWith('!ruling')) && !(msg.content.startsWith('!dice')) &&
+            !(msg.content.startsWith('!roll'))) {
+                msg.reply(', it looks like you have a typo in your command.\nType !commands to see what I can do for you.');
+        }
     }
 })*/
 
 // display all commands
 client.on('message', msg => {
+    if (msg.content.startsWith('!')) {
     if (msg.content.startsWith('!commands')) {
         msg.reply(', here is a list of what I can do:\n!card cardname -> displays card for a given cardname\n' + 
                   '!legality cardname -> displays a cards legalities for Standard, Modern & Commander\n' + 
                   '!rulings cardname -> displays existing rulings for a card\n' + 
                   '!dice xdx -> rolls the specified number of dice for you. !roll works as well');
-    }
+    }}
 })
 
 // roll a specified number of specified dice with !dice xdx
